86c5dfea1d6dbe3e56bd325e1d06655a2a9607f0
R1an <r1ankeel@gmail.com>
2020-10-03 03:07:18 +0300

    start proj

--- Task-Manager/src/app/app.module.ts
+++ Task-Manager/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskComponent } from './task/task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Task-Manager/src/app/task-form/task-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.scss']
+})
+export class TaskFormComponent implements OnInit {
+
+  title = ''
+  text = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(event) {
+    console.log(event.target);
+  }
+
+}
